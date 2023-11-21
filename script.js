aa5bb0ce54a87b7568dfd68db2e7650a33ae3766
Mino <bryanyoba2000@gmail.com>
2023-11-21 17:45:33 -0500

    v1.1

--- script.js
+++ script.js
@@ -1,25 +1,50 @@
 let circularProgressList = document.querySelectorAll(".circular-progress");
+
+
+let options = {
+  root: null,
+  rootMargin: "0px",
+  threshold: 1 
+};
+
+
+let handleIntersection = (entries, observer) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      animateCircularProgress(entry.target);
+      observer.unobserve(entry.target); 
+    }
+  });
+};
+
+
+let observer = new IntersectionObserver(handleIntersection, options);
+
+
 circularProgressList.forEach(circularProgress => {
+  observer.observe(circularProgress);
+});
 
-let progressStarValue = 0;
-let progressEndValue = parseInt(circularProgress.parentElement.getAttribute("data-percent"), 10) || 75;
-let speed = 30;
 
-let progress = setInterval(() => {
-  progressStarValue++;
-  circularProgress.querySelector(".progress-bar").textContent = `${progressStarValue}%`;
+function animateCircularProgress(circularProgress) {
+  let progressStarValue = 0;
+  let progressEndValue = parseInt(circularProgress.parentElement.getAttribute("data-percent"), 10) || 75;
+  let speed = 30;
 
-  circularProgress.style.background = `conic-gradient(#063f5f ${progressStarValue * 3.6}deg, #c8dadf 0deg)`;
+  let progress = setInterval(() => {
+    progressStarValue++;
+    circularProgress.querySelector(".progress-bar").textContent = `${progressStarValue}%`;
 
- 
-if (progressStarValue === progressEndValue) {
+    circularProgress.style.background = `conic-gradient(#063f5f ${progressStarValue * 3.6}deg, #c8dadf 0deg)`;
+
+    if (progressStarValue === progressEndValue) {
       clearInterval(progress);
     }
 
-  
     console.log(progressStarValue);
   }, speed);
-});
+}
+
 
 document.getElementById("descargar").addEventListener("click", () => {
   const xhr = new XMLHttpRequest();
